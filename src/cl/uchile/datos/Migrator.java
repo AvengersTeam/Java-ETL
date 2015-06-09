84a1ecf85b65fed200fd1ad2938a4cccda75be64
Fernando <fgmoralesc@gmail.com>
2015-06-09 13:04:49 -0300

    ETL de eventos funcionando y casi listo, también cambios en Migrator

--- src/cl/uchile/datos/Migrator.java
+++ src/cl/uchile/datos/Migrator.java
@@ -19,9 +19,13 @@ public class Migrator {
 	 */
 	public static void main(String[] args) throws XMLStreamException, FileNotFoundException {
 		String inputFilename = "input/autoridades.xml";
-		String outputFilename = "output/autoridades_output.xml";
+		String outputFilename = "output/autoridades-personas.rdf";
 		PersonETL p = new PersonETL(inputFilename, outputFilename);
 		p.parse();
+		inputFilename = "input/autoridades-eventos.xml";
+		outputFilename = "output/autoridades-eventos.rdf";
+		EventETL e = new EventETL(inputFilename, outputFilename);
+		e.parse();
 	}
 
 }
