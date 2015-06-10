befd36758075c7f37218d7c30e7ce48e7e90818e
Rodolfo Avila <fito.lord.blader@gmail.com>
2015-06-10 16:46:55 -0300

    obra2.0

--- src/cl/uchile/datos/ObraETL.java
+++ src/cl/uchile/datos/ObraETL.java
@@ -4,6 +4,8 @@
 package cl.uchile.datos;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
 import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 
@@ -19,8 +21,9 @@ public class ObraETL extends AbstractETL {
 	 * @throws FileNotFoundException
 	 * 
 	 */
-	public ObraETL(String inputFilename, String outputFilename) throws FileNotFoundException, XMLStreamException {
-		super(inputFilename, outputFilename);
+	//ArrayList<String> outputFilenames=new ArrayList<String>(){{add(outputFilename);add(outputFilename2);add(outputFilename3);}};
+	public ObraETL(String inputFilename, ArrayList<String> outputFilenames) throws FileNotFoundException, XMLStreamException {
+		super(inputFilename, outputFilenames);
 	}
 
 	/**
@@ -32,43 +35,39 @@ public class ObraETL extends AbstractETL {
 		String base_uri = "http://datos.uchile.cl/recurso/";
 		
 		String owlUri = "http://datos.uchile.cl/ontologia/";
-		String foafUri = "http://xmlns.com/foaf/0.1/";
-		String bioUri = "http://vocab.org/bio/0.1/";
 		String rdfUri = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
 		String dcUri = "http://purl.org/dc/elements/1.1/";
 		String dctUri = "http://purl.org/dc/terms/";
 		String frbrerUri = "http://iflastandards.info/ns/fr/frbr/frbrer#";
 
-		this.writer.writeStartDocument();
-		this.writer.setPrefix("rdf", rdfUri);
+		this.writers.get(0).writeStartDocument();
+		this.writers.get(0).setPrefix("rdf", rdfUri);
 		this.writer.writeStartElement(rdfUri, "RDF");
 		// XML namespaces
-		this.writer.writeNamespace("dc",dcUri);
-		this.writer.writeNamespace("dct",dctUri);
-		this.writer.writeNamespace("frbrer",frbrerUri);
-		this.writer.writeNamespace("owl", owlUri);
-		this.writer.writeNamespace("foaf", foafUri);
-		this.writer.writeNamespace("bio", bioUri);
-		this.writer.writeNamespace("rdf", rdfUri);
+		this.writers.get(0).writeNamespace("dc",dcUri);
+		this.writers.get(0).writeNamespace("dct",dctUri);
+		this.writers.get(0).writeNamespace("frbrer",frbrerUri);
+		this.writers.get(0).writeNamespace("owl", owlUri);		
+		this.writers.get(0).writeNamespace("rdf", rdfUri);
 		
 		boolean isFirst = true;
 	
 		while(this.reader.hasNext()) {
 			if (this.reader.next() != XMLStreamConstants.START_ELEMENT) continue; 
 			tagname = this.reader.getName().toString();	
-			String attributeValue = this.reader.getAttributeValue("", "type");
+			String attributeValueType = this.reader.getAttributeValue("", "type");
+			String attributeValueName = this.reader.getAttributeValue("", "name");
 			if (!tagname.equals( "asset" ) &&  !(isFirst)) continue;
-			try {if (attributeValue.equals("FOLDER")) continue;}
+			try {if (attributeValueType.equals("FOLDER")) continue;}
 			catch(Exception e){}
 			w = this.reader.getAttributeCount();
 			System.out.println(tagname);
 			System.out.println(w);
 			
 			// we assume that CHARACTERS event comes next always, which could not be true
-			System.out.println(attributeValue);
+			System.out.println(attributeValueType);
 			this.reader.next();
-			try {if (attributeValue.equals("ASSET")) {
-				
+			try {if (attributeValueType.equals("ASSET")) {				
 				//if (!isFirst) this.writer.writeEndElement();
 				isFirst = false;
 			}}
@@ -78,19 +77,61 @@ public class ObraETL extends AbstractETL {
 				id = this.reader.getText();
 				System.out.println(id);
 				//if (!isFirst) this.writer.writeEndElement();
+				this.writers.get(0).flush();			
+				this.writers.get(0).setPrefix("owl", owlUri);
+				this.writers.get(0).writeStartElement(owlUri, "NamedIndividual");
+				this.writers.get(0).writeAttribute(rdfUri, "about", base_uri + "obra/" + id);
+				this.writers.get(0).writeEmptyElement(rdfUri, "type");
+				this.writers.get(0).writeAttribute(rdfUri, "resource", "frbrer:C1001");
+				// Agregar link a expresion
+				this.writer.writeEmptyElement(frbrerUri, "isRealizedThrough");
+				this.writer.writeAttribute(rdfUri, "resource",base_uri + "expresion/" + id);
 				isFirst = false;
 			}
-				// We should not close the parent element
-				
-				// Write buffered element, this can be optimized
-				this.writer.flush();
-				// New guy starts here
-				this.writer.setPrefix("owl", owlUri);
-				this.writer.writeStartElement(owlUri, "NamedIndividual");
-				this.writer.writeAttribute(rdfUri, "about", base_uri + "autoridad/" + id);
-			
-			
-			if(attributeValue == null) continue;
+			try{ 
+				if(attributeValueName.equals("Title")){			
+					System.out.println("el title es: ");
+					String title = this.reader.getText();
+					System.out.println(title);
+					this.writers.get(0).flush();
+					this.writers.get(0).writeAttribute(dcUri,"title",title);
+					isFirst=false;
+				}
+			}
+			catch(Exception e){}
+			try{ 
+				if(attributeValueName.equals("NAME")){			
+					System.out.println("el NAME es: ");
+					String NAME = this.reader.getText();
+					System.out.println(NAME);
+					this.writers.get(0).flush();
+					this.writers.get(0).writeAttribute(dctUri,"alternative",NAME);
+					isFirst=false;
+				}
+			}
+			catch(Exception e){}
+			try{ 
+				if(attributeValueName.equals("Date")){			
+					System.out.println("el Date es: ");
+					String Date = this.reader.getText();
+					String Date2;
+					System.out.println(Date);
+					if(Date.substring(0, 6)=="[entre")
+						Date2 = Date.substring(7, 11)+" - "+Date.substring(14,18)+" rango estimado";
+					else{
+						if(Date.contains("?"))
+							Date2 = Date.substring(0,2)+"00 fecha estima";
+						else
+							Date2 = Date;
+					}						
+					this.writers.get(0).flush();
+					this.writers.get(0).writeAttribute(dctUri,"issued",Date2);
+					isFirst=false;
+				}
+			}
+			catch(Exception e){}			
+						
+			if(attributeValueName == null) continue;
 			/* por terminar
 			if(attributeValue.equals("100") && this.reader.getText().contains("|a")) {
 				String text = this.reader.getText();
