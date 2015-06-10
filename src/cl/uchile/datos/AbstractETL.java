b2df9229cb33bd3d0665d616f00d9057d759b75c
karlitrotski <cpdettoni@gmail.com>
2015-06-10 15:43:48 -0300

    Abstract modificado para que los ETL puedan tener más de un output

--- src/cl/uchile/datos/AbstractETL.java
+++ src/cl/uchile/datos/AbstractETL.java
@@ -6,6 +6,8 @@ package cl.uchile.datos;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
@@ -19,21 +21,34 @@ import javax.xml.stream.XMLStreamWriter;
 public abstract class AbstractETL {
 	
 	XMLInputFactory inputFactory;
-	XMLOutputFactory outputFactory;
 	XMLStreamReader reader;
+	XMLOutputFactory outputFactory;
 	XMLStreamWriter writer;
+	ArrayList<XMLStreamWriter> writers;
 	
 	/**
 	 * @throws XMLStreamException 
 	 * @throws FileNotFoundException 
 	 * 
 	 */
+	/* Caso varios outputs */
+	public AbstractETL(String inputFilename, ArrayList<String> outputFilenames) throws XMLStreamException, FileNotFoundException {
+		this.inputFactory = XMLInputFactory.newInstance();
+		this.reader = this.inputFactory.createXMLStreamReader(new FileInputStream(inputFilename), "UTF8");
+		this.outputFactory = XMLOutputFactory.newInstance();
+		for(String output : outputFilenames) {
+			XMLStreamWriter aux = this.outputFactory.createXMLStreamWriter(new FileOutputStream(output), "UTF8");
+			this.writers.add(aux);
+		}
+	}
+	/* Caso un solo output */
 	public AbstractETL(String inputFilename, String outputFilename) throws XMLStreamException, FileNotFoundException {
 		this.inputFactory = XMLInputFactory.newInstance();
-		this.outputFactory = XMLOutputFactory.newInstance();
 		this.reader = this.inputFactory.createXMLStreamReader(new FileInputStream(inputFilename), "UTF8");
+		this.outputFactory = XMLOutputFactory.newInstance();
 		this.writer = this.outputFactory.createXMLStreamWriter(new FileOutputStream(outputFilename), "UTF8");
 	}
+
 
 	
 }
